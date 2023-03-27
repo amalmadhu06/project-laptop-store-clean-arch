b1dbfce9e7844bb676057d392d57d187592a16d3
amalmadhu06 <amalmadhu06@gmail.com>
2023-03-27 23:25:10 +0530

    refactor package names

--- pkg/repository/cart.go
+++ pkg/repository/cart.go
@@ -299,7 +299,7 @@ func (c *cartDatabase) ViewCart(ctx context.Context, userID int) (modelHelper.Vi
 	finalCart.Discount = cartDetails.Discount
 	finalCart.CartTotal = finalCart.SubTotal - finalCart.Discount
 	finalCart.CartItems = allItems
-
+	fmt.Println(finalCart)
 	return finalCart, nil
 }
 
