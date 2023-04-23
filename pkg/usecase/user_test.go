9d184046eeb8c199699f69422b0b634ad21d0f99
amalmadhu06 <amalmadhu06@gmail.com>
2023-04-23 16:48:38 +0530

    add unit test : user usecase

--- pkg/usecase/user_test.go
+++ pkg/usecase/user_test.go
@@ -142,3 +142,183 @@ func TestCreateUser(t *testing.T) {
 	}
 
 }
+
+func TestLoginWithEmail(t *testing.T) {
+	//NewController from gomock package returns a new controller for testing
+	ctrl := gomock.NewController(t)
+
+	// NewMockUserRepository creates a new mockRepo instance
+	userRepo := mockRepo.NewMockUserRepository(ctrl)
+
+	// NewMockUserRepository creates a new mockRepo instance
+	orderRepo := mockRepo.NewMockOrderRepository(ctrl)
+
+	userUseCase := NewUserUseCase(userRepo, orderRepo)
+
+	// testData is a slice of struct which holds multiple test cases
+	testData := []struct {
+		name           string
+		input          model.UserLoginEmail
+		buildStub      func(userRepo mockRepo.MockUserRepository)
+		expectedOutput model.UserDataOutput
+		expectedError  error
+	}{
+		{
+			name: "correct email and password",
+			input: model.UserLoginEmail{
+				Email:    "amalmadhu@gmail.com",
+				Password: "amal@123",
+			},
+			buildStub: func(userRepo mockRepo.MockUserRepository) {
+				hashedPassword, err := bcrypt.GenerateFromPassword([]byte("amal@123"), 10)
+				if err != nil {
+					t.Fatalf("failed to generate hash from password in build stub %v", err)
+				}
+				userRepo.EXPECT().
+					FindByEmail(
+						gomock.Any(),
+						"amalmadhu@gmail.com").
+					Times(1).
+					Return(model.UserLoginVerifier{
+						ID:         1,
+						FName:      "Amal",
+						LName:      "Madhu",
+						Email:      "amalmadhu@gmail.com",
+						Phone:      "7902631234",
+						Password:   string(hashedPassword),
+						IsBlocked:  false,
+						IsVerified: true,
+					}, nil)
+			},
+			expectedOutput: model.UserDataOutput{
+				ID:    1,
+				FName: "Amal",
+				LName: "Madhu",
+				Email: "amalmadhu@gmail.com",
+				Phone: "7902631234",
+			},
+			expectedError: nil,
+		},
+		{
+			name: "incorrect email or password",
+			input: model.UserLoginEmail{
+				Email:    "randomEmail@gmail.com",
+				Password: "randomPassword",
+			},
+			buildStub: func(userRepo mockRepo.MockUserRepository) {
+				userRepo.EXPECT().
+					FindByEmail(
+						gomock.Any(),
+						"randomEmail@gmail.com").
+					Times(1).
+					Return(
+						model.UserLoginVerifier{},
+						errors.New("error finding userData"),
+					)
+			},
+			expectedOutput: model.UserDataOutput{},
+			expectedError:  errors.New("error finding userData"),
+		},
+	}
+
+	// looping through the test cases and running them individually
+	for _, tt := range testData {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.buildStub(*userRepo)
+			_, actualData, actualErr := userUseCase.LoginWithEmail(context.TODO(), tt.input)
+			assert.Equal(t, actualData, tt.expectedOutput)
+			assert.Equal(t, tt.expectedError, actualErr)
+		})
+	}
+}
+
+func TestLoginWithPhone(t *testing.T) {
+	//NewController from gomock package returns a new controller for testing
+	ctrl := gomock.NewController(t)
+
+	// NewMockUserRepository creates a new mockRepo instance
+	userRepo := mockRepo.NewMockUserRepository(ctrl)
+
+	// NewMockUserRepository creates a new mockRepo instance
+	orderRepo := mockRepo.NewMockOrderRepository(ctrl)
+
+	userUseCase := NewUserUseCase(userRepo, orderRepo)
+
+	// testData is a slice of struct which holds multiple test cases
+	testData := []struct {
+		name           string
+		input          model.UserLoginPhone
+		buildStub      func(userRepo mockRepo.MockUserRepository)
+		expectedOutput model.UserDataOutput
+		expectedError  error
+	}{
+		{
+			name: "correct phone and password",
+			input: model.UserLoginPhone{
+				Phone:    "7902638843",
+				Password: "amal@123",
+			},
+			buildStub: func(userRepo mockRepo.MockUserRepository) {
+				hashedPassword, err := bcrypt.GenerateFromPassword([]byte("amal@123"), 10)
+				if err != nil {
+					t.Fatalf("failed to generate hash from password in build stub %v", err)
+				}
+				userRepo.EXPECT().
+					FindByPhone(
+						gomock.Any(),
+						"7902638843",
+					).
+					Times(1).
+					Return(model.UserLoginVerifier{
+						ID:         1,
+						FName:      "Amal",
+						LName:      "Madhu",
+						Email:      "amalmadhu@gmail.com",
+						Phone:      "7902631234",
+						Password:   string(hashedPassword),
+						IsBlocked:  false,
+						IsVerified: true,
+					}, nil)
+			},
+			expectedOutput: model.UserDataOutput{
+				ID:    1,
+				FName: "Amal",
+				LName: "Madhu",
+				Email: "amalmadhu@gmail.com",
+				Phone: "7902631234",
+			},
+			expectedError: nil,
+		},
+		{
+			name: "incorrect phone and password",
+			input: model.UserLoginPhone{
+				Phone:    "8956456987",
+				Password: "randompassword",
+			},
+			buildStub: func(userRepo mockRepo.MockUserRepository) {
+
+				userRepo.EXPECT().
+					FindByPhone(
+						gomock.Any(),
+						"8956456987").
+					Times(1).
+					Return(
+						model.UserLoginVerifier{},
+						errors.New("error finding userData"),
+					)
+			},
+			expectedOutput: model.UserDataOutput{},
+			expectedError:  errors.New("error finding userData"),
+		},
+	}
+
+	// looping through the test cases and running them individually
+	for _, tt := range testData {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.buildStub(*userRepo)
+			_, actualData, actualErr := userUseCase.LoginWithPhone(context.TODO(), tt.input)
+			assert.Equal(t, actualData, tt.expectedOutput)
+			assert.Equal(t, tt.expectedError, actualErr)
+		})
+	}
+}
